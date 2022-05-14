bcd4d9291a5d0d26f64cdeb3f2d123aad2561f87
Anuj16091996 <anuj161996@gmail.com>
2022-05-13 23:56:56 -0400

    Created Previous history compoenent

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Slider from "./Slider";
 import PreviousHistory from "./PreviousHistory";
 import "./CSS/home.css";
-import { useState, useEffect } from "react";
 
 function Home(props) {
   return (
     <div className={props.screenSize.width > 1800 ? "container" : ""}>
       <Slider />
-      {console.log()}
-      <PreviousHistory />
+      <PreviousHistory screenSize={props.screenSize} />
     </div>
   );
 }
--- frontend/src/components/PreviousHistory.jsx
+++ frontend/src/components/PreviousHistory.jsx
@@ -4,18 +4,95 @@ import "./CSS/previous.css";
 
 function PreviousHistory(props) {
   return (
-    <div className="mb-5 col-xl-4 ">
-      <Card className="card-design ">
-        <Card.Img variant="top" src={banner_one} />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+    <div className="container">
+      <div className="row no-gutters ">
+        <div className="col-sm-2 col-md-4 col-lg-3 mb-5">
+          <Card
+            className={
+              props.screenSize.width > 1023
+                ? "shadow p-3 mb-5 bg-white rounded on-top-element"
+                : "shadow p-3 mb-5 bg-white rounded"
+            }
+          >
+            <h4>Keep shopping for</h4>
+            <Card.Img variant="top" src={banner_one} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div className="col-sm-2 col-md-4 col-lg-3">
+          <Card
+            className={
+              props.screenSize.width > 1023
+                ? "shadow p-3 mb-5 bg-white rounded on-top-element"
+                : "shadow p-3 mb-5 bg-white rounded"
+            }
+          >
+            <h4>Buy Again</h4>
+            <Card.Img variant="top" src={banner_one} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div className="col-sm-2 col-md-4 col-lg-3">
+          <Card
+            className={
+              props.screenSize.width > 1023
+                ? "shadow p-3 mb-5 bg-white rounded on-top-element"
+                : "shadow p-3 mb-5 bg-white rounded"
+            }
+          >
+            <h4>Today's Deal</h4>
+            <Card.Img variant="top" src={banner_one} />
+
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div className="col-sm-2 col-md-4 col-lg-3">
+          <Card
+            className={
+              props.screenSize.width > 1023
+                ? "shadow p-3 mb-5 bg-white rounded on-top-element"
+                : "shadow p-3 mb-5 bg-white rounded"
+            }
+          >
+            <h4>Looking for a gift?</h4>
+            <p>Find personalized gift ideas for everyone on your list.</p>
+            {/* <Card.Img variant="top" src={banner_one} />
+
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body> */}
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
